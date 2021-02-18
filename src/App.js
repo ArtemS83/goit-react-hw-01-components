943f8e2db2f2824e07bf659201f3144107fafa22
Atrem Sergieiev <a_serg83@ukr.net>
2021-02-18 23:18:10 +0100

    task-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
-  );
-}
+import React from 'react';
+import Layout from './components/Layout/Layout';
+import Panel from './components/Panel/Panel';
+import Task1 from './components/Task1/Task1';
+
+const App = () => (
+  <Layout>
+    <h1 style={{ marginBottom: 15 }}>Hello React</h1>
+    <Panel title="Task №1-social profile">
+      <Task1 />
+    </Panel>
+
+    <Panel title="Task №2"></Panel>
+    <Panel title="Task №3"></Panel>
+    <Panel title="Task №4"></Panel>
+  </Layout>
+);
 
 export default App;
